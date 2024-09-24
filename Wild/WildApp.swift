572a3f946130189f8f822cdfdcfd1cbce5743f70
David Viloria Ortega <david.viloria0424@gmail.com>
2024-09-24 00:10:16 -0500

    Initial Commit

---
+++ Wild/WildApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildApp.swift
+//  Wild
+//
+//  Created by David Viloria Ortega on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
