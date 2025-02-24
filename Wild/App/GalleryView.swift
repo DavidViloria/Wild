3918bc9c7dca3eb3712d7582f191d0543debad04
David Viloria Ortega <david.viloria@citi.com.mx>
2025-02-23 21:47:35 -0500

    Improve Gallery View

--- Wild/App/GalleryView.swift
+++ Wild/App/GalleryView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .heavy)
+    //    let gridLayout: [GridItem] = [
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible())
+    //    ]
+    
+    //    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    @State private var gridLayout : [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwicht(){
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            
+            VStack(alignment: .center, spacing: 30) {
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding()
+                    .onChange(of: gridColumn) { newValue in
+                        gridSwicht()
+                    }
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 0.9))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    }
+                }
+                .animation(.bouncy)
+                .onAppear {
+                    gridSwicht()
+                }
+            }
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
+    
+    
 }
 
 #Preview {
